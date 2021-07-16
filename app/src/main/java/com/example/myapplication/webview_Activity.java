9d55027e4376117b888b92c0c7295dd2a62c4e10
pLe98 <lelkadaparami@gmail.com>
2021-07-16 22:28:13 +0530

    webview changes

--- app/src/main/java/com/example/myapplication/webview_Activity.java
+++ app/src/main/java/com/example/myapplication/webview_Activity.java
@@ -7,13 +7,21 @@ import android.webkit.WebView;
 
 public class webview_Activity extends AppCompatActivity {
 
-    WebView myWebView = (WebView) findViewById(R.id.webview);
+    wv1=(WebView)findViewById(R.id.webView);
+      wv1.setWebViewClient(new MyBrowser());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
-        myWebView.loadUrl("http://www.example.com");
+        wv1.loadUrl(url);
 
     }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
 }
